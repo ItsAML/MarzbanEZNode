23571afb3996593a096201ae26536f80d8f7ba06
Mohammad <mazhari0546@gmail.com>
2023-12-22 01:20:37 +0330

    fixed issue that causes commands to not be executed

--- curlscript.py
+++ curlscript.py
@@ -126,15 +126,11 @@ cert_info = get_cert(access_token)
 commands = [
     'sudo ufw disable',
     'curl -fsSL https://get.docker.com | sh',
-    '[ -d "Marzban-node" ] && rm -r "Marzban-node"',
+    '[ -d Marzban-node ] && rm -r Marzban-node',
     'git clone https://github.com/Gozargah/Marzban-node',
-    'cd Marzban-node',
-    'docker compose up -d',
-    'docker compose down',
-    f'echo "{cert_info}" > /var/lib/marzban-node/ssl_client_cert.pem',
-    'rm docker-compose.yml',
-    'echo \'services:\n  marzban-node:\n    image: gozargah/marzban-node:latest\n    restart: always\n    network_mode: host\n    environment:\n      SSL_CERT_FILE: "/var/lib/marzban-node/ssl_cert.pem"\n      SSL_KEY_FILE: "/var/lib/marzban-node/ssl_key.pem"\n      SSL_CLIENT_CERT_FILE: "/var/lib/marzban-node/ssl_client_cert.pem"\n    volumes:\n      - /var/lib/marzban-node:/var/lib/marzban-node\' > docker-compose.yml',
-    'docker compose up'
+    'cd Marzban-node && docker compose up -d && docker compose down && rm docker-compose.yml',
+    f'sudo echo "{cert_info}" > /var/lib/marzban-node/ssl_client_cert.pem',
+    'cd Marzban-node && sudo echo \'services:\n  marzban-node:\n    image: gozargah/marzban-node:latest\n    restart: always\n    network_mode: host\n    environment:\n      SSL_CERT_FILE: "/var/lib/marzban-node/ssl_cert.pem"\n      SSL_KEY_FILE: "/var/lib/marzban-node/ssl_key.pem"\n      SSL_CLIENT_CERT_FILE: "/var/lib/marzban-node/ssl_client_cert.pem"\n    volumes:\n      - /var/lib/marzban-node:/var/lib/marzban-node\' > docker-compose.yml && docker compose up -d'
 ]
 
 # Establish SSH connection
